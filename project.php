b4fbabcd9dec6c8bd0b37a63a32027e2705df073
Andrew Crudge <andcru@gmail.com>
2013-04-28 13:37:49 -0400

    Simple project page added

--- project.php
+++ project.php
@@ -1,7 +1,14 @@
 <?php
 $proj = $dbc->query("SELECT * FROM projects JOIN files ON projects.photo = files.id WHERE projects.id='{$pg_id}'")->fetch_assoc();
+$stud = new student($proj['cuid']);
 ?>
 
 <h2><?php echo $proj['title']; ?></h2>
 
-<p><img src="/users/<?php echo $proj['cuid']; ?>/uploads/<?php echo $proj['name']; ?>"></p>
+<p><b>Student: <?php echo $stud->name(); ?></b></p>
+
+<p>Image: <img src="/users/<?php echo $proj['cuid']; ?>/uploads/<?php echo $proj['name']; ?>"></p>
+
+<p>Project Description:</p>
+
+<p><?php echo $proj['content']; ?></p>
