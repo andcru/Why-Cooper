fd59cb2460d29ad230379dd6f7931db87d240a57
Andrew Crudge <andcru@gmail.com>
2013-04-29 14:11:13 -0400

    Test

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 
 <p>This site is currently under construction. <b>Students: Click <a href="login">here</a> to login!</b></p>
 <!--
-<h3>See prototype website below: </h3>
+<h3>See prototype website below:</h3>
 <div>
 	<img src="/img/rev1.jpg">
 </div>
