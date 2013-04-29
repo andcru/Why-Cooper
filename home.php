495c84992e401b2f0fffc70fa14fff5bebbdd5e9
Andrew Crudge <andcru@gmail.com>
2013-04-29 14:16:15 -0400

    I'm dumb

--- home.php
+++ home.php
@@ -1,8 +1,9 @@
 <h3>Why Cooper Union: Home Page</h3>
 
 <p>This site is currently under construction. <b>Students: Click <a href="login">here</a> to login!</b></p>
-
-<h3>See prototype website below: </h3>
+<!--
+<h3>See prototype website below:</h3>
 <div>
 	<img src="/img/rev1.jpg">
 </div>
+-->
