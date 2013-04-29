0898fd487c4778207dab7f3432183f5e4a2f52c0
Andrew Crudge <andcru@gmail.com>
2013-04-29 02:07:06 -0400

    Added prototype to home page

--- home.php
+++ home.php
@@ -1,3 +1,8 @@
 <h3>Why Cooper Union: Home Page</h3>
 
 <p>This site is currently under construction. <b>Students: Click <a href="login">here</a> to login!</b></p>
+
+<h3>See prototype website below: </h3>
+<div>
+	<img src="/img/rev1.jpg">
+</div>
