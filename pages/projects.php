c92e774a4931f1b7f2a838a762b45aec44575fae
Andrew Crudge <andcru@gmail.com>
2013-05-03 00:39:52 -0400

    Added login to projects page

--- pages/projects.php
+++ pages/projects.php
@@ -7,8 +7,10 @@
 	</ul>
 </div>
 
-<h1>Project Showcase</h1>
-<p>Curious to see how Cooper Union students use their free education?</p>
-<p>Browse projects made by students and alumni from all three schools.</p>
+<div>
+	<h1>Project Showcase</h1>
+	<p>Curious to see how Cooper Union students use their free education? Browse projects made by students and alumni from all three schools.</p>
+	<p>Members of the school community: <a href="/login">Login</a> to submit your Cooper Experience!</p>
+</div>
 
 <div id="all_projects" style=""></div>
