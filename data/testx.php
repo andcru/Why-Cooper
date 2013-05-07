e20619ff807d06b243f380bc20dd17c1c220b1bb
Andrew Crudge <andcru@gmail.com>
2013-05-07 16:01:38 -0400

    Changed statement

--- data/testx.php
+++ data/testx.php
@@ -1,8 +1,7 @@
 <?php
-/*
 $sc = $_SESSION['cuid'];
 $_SERVER['DOCUMENT_ROOT'] = '/var/www/why';
-$res = $dbc->query("SELECT projects.id,files.cuid,files.name FROM projects JOIN files ON projects.photo = files.id");
+$res = $dbc->query("SELECT projects.id,files.cuid,files.name FROM projects JOIN files ON projects.photo = files.id WHERE projects.id = 79");
 while($r = $res->fetch_assoc()) {
 	print_r($r);
 	echo "<br/>";
@@ -11,4 +10,3 @@ while($r = $res->fetch_assoc()) {
 	make_thumb($r['name'],"500",$file[1],$file[0]."_.".$file[1]);
 }
 $_SESSION['cuid'] = $sc;
-*/
